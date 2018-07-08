532f252d04d008e25a6d7469ccb18ea02fd89304
Exarchos Dimitrios <34217805+exdimitris@users.noreply.github.com>
2018-07-08 23:35:39 +0300

    Add files via upload

---
+++ config/config.php
@@ -0,0 +1,10 @@
+<?php
+/* ========================================================
+ * Open eClass 3.0 configuration file
+ * Created by install on 2017-12-03 21:08
+ * ======================================================== */
+
+$mysqlServer = 'localhost';
+$mysqlUser = 'root';
+$mysqlPassword = '';
+$mysqlMainDb = 'eclass2';
